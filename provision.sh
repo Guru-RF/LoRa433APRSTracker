0617b28dff00a2762a6a71d79704d1abdb7ded90
Joeri Van Dooren <ure@moreorless.be>
2024-04-28 10:32:44 +0200

    adpot to vscode circuitpython environment

--- provision.sh
+++ provision.sh
@@ -27,14 +27,14 @@ fi
 DIR="/Volumes/CIRCUITPY"
 if [ -d "$DIR" ]; then
   echo "Install software in ${DIR}..."
-  cp -r src/lib /Volumes/CIRCUITPY
-  cp src/boot.py /Volumes/CIRCUITPY
+  cp -r lib /Volumes/CIRCUITPY
+  cp boot.py /Volumes/CIRCUITPY
   echo "1" > /Volumes/CIRCUITPY/sequence
-  cp src/config.py /tmp
+  cp config.py /tmp
   perl -i -pe "s/--CALL--/${args[0]}/g" /tmp/config.py
   perl -i -pe "s/--BOOL--/${args[1]}/g" /tmp/config.py
   cp -f /tmp/config.py /Volumes/CIRCUITPY
-  cp src/code.py /Volumes/CIRCUITPY
+  cp code.py /Volumes/CIRCUITPY
   rm -f /tmp/config.py
   sync
   diskutil unmount /Volumes/CIRCUITPY
@@ -46,9 +46,9 @@ if [ -d "$DIR" ]; then
   echo "Updating software in ${DIR}..."
   unlink /Volumes/APRSTRKR/ro
   sync
-  cp -r src/lib /Volumes/APRSTRKR
-  cp src/boot.py /Volumes/APRSTRKR
-  cp src/code.py /Volumes/APRSTRKR
+  cp -r lib /Volumes/APRSTRKR
+  cp boot.py /Volumes/APRSTRKR
+  cp code.py /Volumes/APRSTRKR
   sync
   diskutil unmount /Volumes/APRSTRKR
   echo "done"
