c11da49b873e4b37ca9cb2e458527d580e097465
Joeri van Dooren <joeri@weepee.org>
2025-05-22 10:48:35 +0200

    update provisioning added reset

--- provision.sh
+++ provision.sh
@@ -25,7 +25,6 @@ if [ -d "$DIR" ]; then
   echo "Install software in ${DIR}..."
   cp -r lib /Volumes/CIRCUITPY
   cp boot.py /Volumes/CIRCUITPY
-  echo "1" > /Volumes/CIRCUITPY/sequence
   cp config.py /tmp
   perl -i -pe "s/--CALL--/${args[0]}/g" /tmp/config.py
   perl -i -pe "s/--MSGS--/${args[1]}/g" /tmp/config.py
@@ -40,11 +39,17 @@ fi
 DIR="/Volumes/APRSTRKR"
 if [ -d "$DIR" ]; then
   echo "Updating software in ${DIR}..."
-  unlink /Volumes/APRSTRKR/ro
   sync
   cp -r lib /Volumes/APRSTRKR
   cp boot.py /Volumes/APRSTRKR
   cp code.py /Volumes/APRSTRKR
+  if [ ! -f "$DIR/config.py" ]; then
+    cp config.py /tmp
+    perl -i -pe "s/--CALL--/${args[0]}/g" /tmp/config.py
+    perl -i -pe "s/--MSGS--/${args[1]}/g" /tmp/config.py
+    cp -f /tmp/config.py /Volumes/APRSTRKR
+    rm -f /tmp/config.py
+  fi
   sync
   diskutil unmount /Volumes/APRSTRKR
   echo "done"
